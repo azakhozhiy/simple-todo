ee184a41659dc7d2aa1c389149fb872d65f0fc78
Alexander <azk@avzare.com>
2020-11-19 11:16:10 +0300

    wip

--- config.example.php
+++ config.example.php
@@ -4,15 +4,8 @@ const ENV_PROD = 'prod';
 const ENV_DEV = 'dev';
 
 return [
-    'env' => 'dev',
-    'dev' => [
-        'db_host' => 'localhost',
-        'db_port' => 5432,
-        'db_database' => 'unlimint',
-        'db_username' => 'homestead',
-        'db_password' => 'secret',
-    ],
-    'prod' => [
+    'app' => [
+        'env' => ENV_DEV,
         'db_host' => 'localhost',
         'db_port' => 5432,
         'db_database' => 'unlimint',
