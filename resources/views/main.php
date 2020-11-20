49f2394af88ab765670b1139d75e5b85e73d1483
Alexander <azk@avzare.com>
2020-11-20 10:48:51 +0300

    fix

--- resources/views/main.php
+++ resources/views/main.php
@@ -1,7 +1,7 @@
 <section class="section tasks mb-5" id="main-page">
   <div class="container-fluid">
     <div class="section__content">
-      <task-page>
+      <task-page></task-page>
     </div>
   </div>
 </section>
